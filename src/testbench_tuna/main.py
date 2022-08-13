e59da358eb423ecd0ebeceecb8b67074b8dfa273
Tim Schwenke <tim.schwenke@trallnag.com>
2022-08-13 22:23:24 +0200

    chore: Misc

--- src/testbench_tuna/main.py
+++ src/testbench_tuna/main.py
@@ -12,4 +12,4 @@ async def root():
 
 @app.get("/pizza-size")
 async def pizza_size(diameter: int | float):
-    return utils.pizza_size(diameter=diameter)
+    return {"area": utils.pizza_size(diameter)}
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,11 @@
-def test_main():
-    pass
+from fastapi.testclient import TestClient
+
+from testbench_tuna import main
+
+client = TestClient(main.app)
+
+
+def test_read_pizza_size():
+    response = client.get("/pizza-size", params={"diameter": 666})
+    assert response.status_code == 200
+    assert response.json() == {"area": 666}
