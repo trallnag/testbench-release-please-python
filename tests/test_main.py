6ba0b89f9b751f4af3a4dfd4aba5aa641e17b5bf
Crispy Mouse <tim+crispymouse@trallnag.com>
2022-08-16 18:17:10 +0200

    squash! Add tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -15,3 +15,20 @@ def test_get_pizza_size():
     response = client.get("/pizza-size", params={"diameter": 666})
     assert response.status_code == 200
     assert response.json() == {"area": 666}
+
+
+def test_get_user_username_email_foo():
+    response = client.get("/users/foo/email")
+    assert response.status_code == 200
+    assert response.json() == {"username": "foo", "email": "foo@example.com"}
+
+
+def test_get_user_username_email_bar():
+    response = client.get("/users/bar/email")
+    assert response.status_code == 200
+    assert response.json() == {"username": "bar", "email": "bar@example.com"}
+
+
+def test_get_user_username_email_404():
+    response = client.get("/users/fefefe/email")
+    assert response.status_code == 404
