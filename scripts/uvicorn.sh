12e738bcf1ecc3d7104e074c409da36455456822
Tim Schwenke <tim.schwenke@trallnag.com>
2022-08-21 13:07:23 +0200

    ci: Adjust workflow asdf caching

--- scripts/uvicorn.sh
+++ scripts/uvicorn.sh
@@ -13,8 +13,8 @@ script_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 
 . "$script_dir/base.sh"
 
-# ------------------------------------------------------------------------------
-
 cd "$PROJECT_DIR" || exit 1
+
+# ------------------------------------------------------------------------------
 
 poetry run uvicorn src.$PACKAGE_NAME.main:app --reload
